4a548430de8685172baa389cc2a047136b724001
Elly <elly@mg.crane.ai>
2018-11-02 15:31:53 -0400

    added tests/create_account refactoring

---
+++ tests/create_account.test.js
@@ -0,0 +1,21 @@
+/* 
+ * Contact: team@fusioncode.ai
+ *
+ *
+ * Project  - create account UNIT TESTS
+ * Application Client
+ * Fri
+*/
+
+/* create account view has a Label located at the mid-bottom center of the view with text sign up */
+it("create account view has a Label located at the mid-bottom center of the view with text sign up", () => {
+  /* ToDo developer needs to complete unit test 
+               create account view has a Label located at the mid-bottom center of the view with text sign up
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+})
