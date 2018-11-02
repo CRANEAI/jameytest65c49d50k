913e9e5394b3e95c54b8ccb03a67b24578b4585a
Elly <elly@mg.crane.ai>
2018-11-02 15:34:08 -0400

    added tests/premier_league refactoring

---
+++ tests/premier_league.test.js
@@ -0,0 +1,21 @@
+/* 
+ * Contact: team@fusioncode.ai
+ *
+ *
+ * Project  - premier league UNIT TESTS
+ * Application Client
+ * jamey test2
+*/
+
+/* premier league view has Table Row located at the mid-bottom across the full width */
+it("premier league view has Table Row located at the mid-bottom across the full width", () => {
+  /* ToDo developer needs to complete unit test 
+               premier league view has Table Row located at the mid-bottom across the full width
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+})
